36300ca7eaf261ea8713a86a9cf8447f56f7583a
suryapratap2312 <87615712+suryapratap2312@users.noreply.github.com>
2023-04-22 11:52:54 +0530

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,56 @@
+class Test:
+    pass
+
+T = Test()
+print(T)
+
+class Example:
+    r = 10
+    name = 'SRM'
+    def display():
+        print("OOPS")
+        
+E = Example()
+E.display()
+
+class Example:
+    r = 10
+    name = 'SRM'
+    def display(self):
+        print("OOPS")
+        
+E = Example()
+E.display()
+
+class Example:
+    r = 10
+    name = 'SRM'
+    def display(self):
+        print("OOPS")
+        
+E = Example()
+E.display()
+print(E.r)
+print(E.name)
+
+class Example:
+    r = 10
+    name = 'SRM'
+    def display(self):
+        print("OOPS")
+        
+E = Example()
+E.display()
+print(Example.r)
+print(Example.name)
+
+class Example:
+    r = 10
+    name = 'SRM'
+    def display(self):
+        print("OOPS")
+        
+E = Example()
+E.display()
+print(Example.r)
+print(Example.name)
